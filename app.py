3a9a17073539ba85525a1e9cb197f98a6a8ae8f8
MileyNT <mileybv190805@gmail.com>
2026-09-08 19:56:09 -0600

    Initial commit - Templates y Web Forms

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from forms import ContactForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'tu_clave_secreta'
+
+@app.route('/')
+def index ():
+    return render_template('index.html')
+
+@app.route('/contact', methods= ['GET', 'POST'])
+def contact ():
+    form = ContactForm()
+    if form.validate_on_submit():
+        flash(f'Mensaje enviado por {form.name.data}!')
+        return redirect(url_for('index'))
+    return render_template('contact.html', form=form)
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class ContactForm(FlaskForm):
+    name = StringField('Nombre', validators=[DataRequired(), Length(min=2, max=50)])
+    email = StringField('Correo electrónico', validators=[DataRequired(), Email()])
+    message = TextAreaField('Mensaje', validators=[DataRequired(),Length(min=0)])
+    submit = SubmitField('Enviar')
